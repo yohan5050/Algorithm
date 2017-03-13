fb71f9c155bba99e7f24c500a40c2ed5564e1852
yohan5050 <yohan5050@naver.com>
2017-03-13 15:22:28 +0900

    BOJ 13304 방 배정

---
+++ 13304.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+
+int main() {
+	int n, k;
+	int stu12=0, stu34w=0, stu34m=0, stu56w=0, stu56m=0;
+
+	scanf("%d %d", &n, &k);
+	while(n--) {
+		int s, y;
+		scanf("%d %d", &s, &y);
+		if(y==1 || y==2) stu12++;
+		else if(y==3 || y==4) {
+			if(s==0) stu34w++;
+			else stu34m++;
+		}
+		else {
+			if(s==0) stu56w++;
+			else stu56m++;
+		}
+	}
+
+	int ans=0;
+	ans+=stu12/k+(stu12%k ? 1 : 0);
+	ans+=stu34w/k+(stu34w%k ? 1 : 0)+stu34m/k+(stu34m%k ? 1 : 0);
+	ans+=stu56w/k+(stu56w%k ? 1 : 0)+stu56m/k+(stu56m%k ? 1 : 0);
+
+	printf("%d\n", ans);
+	return 0;
+}
